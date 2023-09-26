192dc7f7384c0fe8e208ad4fd5d6f01eca69812d
Emmanuel Amanga <emmanuelamanga2013@gmail.com>
2023-09-26 09:46:38 +0300

    Initial Commit

---
+++ src/components/Results.vue
@@ -0,0 +1,35 @@
+
+
+<template>
+  <div class="result" >
+    <div class="title">{{  results[resultIndex].title }}</div>
+    <div class="desc">
+      {{ results[resultIndex].desc }}
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Results",
+  props: ['results', 'totalCorrect'],
+  computed: {
+    resultIndex(){
+      let index = 0;
+
+      this.results.forEach((elem, i) => {
+        if(elem.min <= this.totalCorrect && elem.max >= this.totalCorrect){
+          index = i;
+        }
+      })
+
+      return index;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
